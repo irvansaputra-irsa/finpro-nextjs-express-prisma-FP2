832ffdf52f9e9952e407f4bd6b81c04f60f057f2
Steven Wen <73114262+loopfree@users.noreply.github.com>
2024-06-07 22:31:47 +0700

    feat: implement add to cart services

---
+++ apps/api/src/services/cartItem.services.ts
@@ -0,0 +1,40 @@
+import { CartItemQuery } from '@/queries/cartItem.queries';
+import Container, { Service } from 'typedi';
+import { checkCartItem } from '@/interfaces/checkCartItem.interfaces';
+import { createCartItem } from '@/interfaces/createCartItem.interfaces';
+import { addCartItem } from '@/interfaces/addCart.interfaces';
+
+@Service()
+export class CartItemService {
+  cartItemQueries = Container.get(CartItemQuery);
+
+  public checkCartItemExistService = async (
+    param: checkCartItem,
+  ): Promise<number> => {
+    try {
+      const data = await this.cartItemQueries.checkCartItemExistQuery(param);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  public createNewCartItemService = async (
+    param: createCartItem,
+  ): Promise<number> => {
+    try {
+      const data = await this.cartItemQueries.createNewCartItemQuery(param);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  public addCartItemService = async (param: addCartItem): Promise<void> => {
+    try {
+      await this.cartItemQueries.addCartItemQuery(param);
+    } catch (error) {
+      throw error;
+    }
+  };
+}

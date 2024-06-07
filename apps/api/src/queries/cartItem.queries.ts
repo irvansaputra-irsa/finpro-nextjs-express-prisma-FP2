8f6d37d52152a48c4ec57cd86856117bb48820e5
Steven Wen <73114262+loopfree@users.noreply.github.com>
2024-06-07 22:25:31 +0700

    feat: implement query to add to cart

---
+++ apps/api/src/queries/cartItem.queries.ts
@@ -0,0 +1,88 @@
+import { Service } from 'typedi';
+import { checkCartItem } from '@/interfaces/checkCartItem.interfaces';
+import { createCartItem } from '@/interfaces/createCartItem.interfaces';
+import { addCartItem } from '@/interfaces/addCart.interfaces';
+import prisma from '@/prisma';
+
+@Service()
+export class CartItemQuery {
+  public checkCartItemExistQuery = async (
+    param: checkCartItem,
+  ): Promise<number> => {
+    try {
+      const { cartId, bookId } = param;
+      const data = await prisma.cartItem.findFirst({
+        where: {
+          book_id: bookId,
+          cart_id: cartId,
+        },
+      });
+
+      if (data === null) {
+        return null;
+      }
+
+      return data.id;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  public createNewCartItemQuery = async (
+    param: createCartItem,
+  ): Promise<number> => {
+    try {
+      const { cartId, bookId } = param;
+      const data = await prisma.cartItem.create({
+        data: {
+          cart_id: cartId,
+          book_id: bookId,
+          quantity: 0,
+          total_price: 0,
+          total_weight: 0,
+        },
+      });
+
+      return data.id;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  public addCartItemQuery = async (param: addCartItem): Promise<void> => {
+    try {
+      const { cartItemId, quantity } = param;
+
+      const cartItemData = await prisma.cartItem.findFirst({
+        where: {
+          id: cartItemId,
+        },
+      });
+
+      const bookData = await prisma.book.findFirst({
+        where: {
+          id: cartItemData.book_id,
+        },
+      });
+
+      const newQuantity = quantity;
+
+      const newPrice = newQuantity * bookData.book_price;
+
+      const newWeight = newQuantity * bookData.book_weight;
+
+      const updateResult = await prisma.cartItem.update({
+        where: {
+          id: cartItemId,
+        },
+        data: {
+          quantity: newQuantity,
+          total_price: newPrice,
+          total_weight: newWeight,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  };
+}

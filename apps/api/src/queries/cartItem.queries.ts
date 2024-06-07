651a0b9479822e3e3a6aae6fe73b44f69a47eb13
Steven Wen <73114262+loopfree@users.noreply.github.com>
2024-06-07 23:35:57 +0700

    feat(query): implement query to update cart item

--- apps/api/src/queries/cartItem.queries.ts
+++ apps/api/src/queries/cartItem.queries.ts
@@ -2,6 +2,7 @@ import { Service } from 'typedi';
 import { checkCartItem } from '@/interfaces/checkCartItem.interfaces';
 import { createCartItem } from '@/interfaces/createCartItem.interfaces';
 import { addCartItem } from '@/interfaces/addCart.interfaces';
+import { updateCartItem } from '@/interfaces/updateCartItem.interfaces';
 import prisma from '@/prisma';
 
 @Service()
@@ -81,6 +82,52 @@ export class CartItemQuery {
           total_weight: newWeight,
         },
       });
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  public updateCartItemQuery = async (param: updateCartItem): Promise<void> => {
+    try {
+      const { cartItemId, quantity } = param;
+
+      const newQuantity = quantity;
+
+      // quantity bernilai negatif dihandle pada frontend
+      if (newQuantity === 0) {
+        await prisma.cartItem.delete({
+          where: {
+            id: cartItemId,
+          },
+        });
+      } else {
+        const cartItemData = await prisma.cartItem.findFirst({
+          where: {
+            id: cartItemId,
+          },
+        });
+
+        const bookData = await prisma.book.findFirst({
+          where: {
+            id: cartItemData.book_id,
+          },
+        });
+
+        const newPrice = newQuantity * bookData.book_price;
+
+        const newWeight = newQuantity * bookData.book_weight;
+
+        const updateResult = await prisma.cartItem.update({
+          where: {
+            id: cartItemId,
+          },
+          data: {
+            quantity: newQuantity,
+            total_price: newPrice,
+            total_weight: newWeight,
+          },
+        });
+      }
     } catch (error) {
       throw error;
     }

3ddeddd6a84dad2bde5793e174e0979c66206d41
Steven Wen <73114262+loopfree@users.noreply.github.com>
2024-06-10 18:37:14 +0700

    feat(cartitems): add cart items controller

---
+++ apps/api/src/controllers/cartItem.controller.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from 'express';
+import Container from 'typedi';
+import { CartItem } from '@prisma/client';
+import { CartItemService } from '@/services/cartItem.services';
+import { BookService } from '@/services/book.services';
+
+export class CartItemController {
+  cartItemController = Container.get(CartItemService);
+  bookController = Container.get(BookService);
+
+  public allController = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const { cartId } = req.body;
+      const returnResult = [];
+      const cartItems: CartItem[] =
+        await this.cartItemController.allCartItemService({
+          cartId,
+        });
+
+      for (let i = 0; i < cartItems.length; i++) {
+        const bookId: number = cartItems[i].book_id;
+        const bookName = await this.bookController.getNameService({ bookId });
+        returnResult.push({
+          name: bookName,
+          quantity: cartItems[i].quantity,
+          totalPrice: cartItems[i].total_price,
+        });
+      }
+
+      res.status(200).json({ returnResult });
+    } catch (error) {
+      throw error;
+    }
+  };
+}

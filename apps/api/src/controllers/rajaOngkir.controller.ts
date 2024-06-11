5534a15b238166dbd75d36777e28deb4628eb10b
Steven Wen <73114262+loopfree@users.noreply.github.com>
2024-06-12 01:29:16 +0700

    feat: create raja ongkir controller

---
+++ apps/api/src/controllers/rajaOngkir.controller.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import Container from 'typedi';
+import { RajaOngkirService } from '@/services/rajaOngkir.services';
+
+export class RajaOngkirController {
+  rajaOngkirController = Container.get(RajaOngkirService);
+
+  public getCityIdController = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const { cityName } = req.body;
+      const cityId = await this.rajaOngkirController.getCityIdService({
+        cityName,
+      });
+
+      res.status(200).json({ cityId });
+    } catch (error) {
+      throw error;
+    }
+  };
+}
